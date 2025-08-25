9fe2b9b2b9dbb447078cff5706fc84b9d7000632
Ryan Nardelli <ryannardelli12@gmail.com>
2025-08-25 11:25:15 -0300

    removing comments of componente AllEpisodes

--- src/components/AllEpisodes/index.tsx
+++ src/components/AllEpisodes/index.tsx
@@ -49,36 +49,4 @@ export function AllEpisodes() {
       </div>
     </div>
   );
-
-    // return (
-    //     // <div className={styles.container}>
-    //     //     {episodes.map(ep => (
-    //     //         <div key={ep.id} className={styles.card}>
-    //     //             {ep.still_path && (
-    //     //                 <img src={`https://image.tmdb.org/t/p/w300${ep.still_path}`} alt={ep.name} />
-    //     //             )}
-    //     //             <div className={styles.info}>
-    //     //                 <h3>{ep.name}</h3>
-    //     //                 <p>Temporada {ep.season_number === 0 ? 1 : ep.season_number} - Episódio {ep.episode_number}</p>
-    //     //                 <p>{ep.overview}</p>
-    //     //             </div>
-    //     //         </div>
-    //     //     ))}
-    //     // </div>
-
-    //     <div className={styles.container}>
-    //     {filteredEpisodes.map(ep => (
-    //       <div key={ep.id} className={styles.card}>
-    //         {ep.still_path && (
-    //           <img src={`https://image.tmdb.org/t/p/w300${ep.still_path}`} alt={ep.name} />
-    //         )}
-    //         <div className={styles.info}>
-    //           <h3>{ep.name}</h3>
-    //           <p>Temporada {ep.season_number === 0 ? 1 : ep.season_number} - Episódio {ep.episode_number}</p>
-    //           <p>{ep.overview}</p>
-    //         </div>
-    //       </div>
-    //     ))}
-    //   </div>
-    // );
 }
