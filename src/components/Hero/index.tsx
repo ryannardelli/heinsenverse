812af0623a9cbe634b24f5ed7b6fb2307e3e0d04
Ryan Nardelli <ryannardelli12@gmail.com>
2025-08-31 20:54:27 -0300

    sendding effect with motion on hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,14 +1,21 @@
 import styles from './styles.module.css';
 
+import { motion } from 'framer-motion';
+
 export function Hero() {
-    return(
+  return(
     <section className={styles.hero}>
       <div className={styles.heroOverlay}>
-        <div className={styles.heroContent}>
+        <motion.div 
+          className={styles.heroContent}
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+        >
           <h1>Heinsenverse</h1>
           <p>Explore tudo sobre a série Breaking Bad em um só lugar</p>
-        </div>
+        </motion.div>
       </div>
     </section>
-    );
+  );
 }
